2a75bb6abc09b38522feb97e5908f0b1a3d0f29d
urkejov1996 <urkejov1996@gmail.com>
2024-06-08 20:36:07 +0200

    added resilience dependencie and circuit breaker controller

--- src/main/java/com/urkejov/currency_exchange_service/controller/CircuitBreakerController.java
+++ src/main/java/com/urkejov/currency_exchange_service/controller/CircuitBreakerController.java
@@ -1,4 +1,13 @@
 package com.urkejov.currency_exchange_service.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CircuitBreakerController {
+
+    @GetMapping("/sample-api")
+    public String sampleApi(){
+        return "Sample API";
+    }
 }
