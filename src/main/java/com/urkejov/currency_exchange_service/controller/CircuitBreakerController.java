e07b42670380690960b6544392a5b7e8750427b9
urkejov1996 <urkejov1996@gmail.com>
2024-06-08 21:12:06 +0200

    added retry and fallback-method with wait duration configuration

--- src/main/java/com/urkejov/currency_exchange_service/controller/CircuitBreakerController.java
+++ src/main/java/com/urkejov/currency_exchange_service/controller/CircuitBreakerController.java
@@ -1,13 +1,27 @@
 package com.urkejov.currency_exchange_service.controller;
 
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CircuitBreakerController {
 
+    private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+
     @GetMapping("/sample-api")
-    public String sampleApi(){
-        return "Sample API";
+    @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+    public String sampleApi() {
+        logger.info("Sample Api call received");
+        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-url", String.class);
+        return forEntity.getBody();
+    }
+
+    private String hardcodedResponse(Exception exception){
+        return "fallback response";
     }
 }
