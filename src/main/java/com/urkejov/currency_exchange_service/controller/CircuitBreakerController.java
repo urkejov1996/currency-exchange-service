4ed995104fbff6fcef3a86b947fdb96128d59a64
urkejov1996 <urkejov1996@gmail.com>
2024-06-12 14:01:38 +0200

    max concurrent calls configured

--- src/main/java/com/urkejov/currency_exchange_service/controller/CircuitBreakerController.java
+++ src/main/java/com/urkejov/currency_exchange_service/controller/CircuitBreakerController.java
@@ -11,24 +11,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import javax.print.attribute.standard.MediaSize;
 
 @RestController
 public class CircuitBreakerController {
-
-    private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
-
     @GetMapping("/sample-api")
-//    @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
-    @CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
-    @RateLimiter(name = "default")
-//    @Bulkhead(name = "default")
+//     @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+//    @CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
+//    @RateLimiter(name = "default")
+    @Bulkhead(name = "default")
     public String sampleApi() {
         logger.info("Sample Api call received");
         ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-url", String.class);
         return forEntity.getBody();
 //        return "sample-api";
+
     }
+
+
+    private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 
     private String hardcodedResponse(Exception exception) {
         return "fallback response";
