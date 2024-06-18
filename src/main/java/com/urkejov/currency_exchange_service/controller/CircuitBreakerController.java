88c1d29383e10317390c5de2c6f8f452fc42e558
urkejov1996 <urkejov1996@gmail.com>
2024-06-18 19:07:01 +0200

    //

--- src/main/java/com/urkejov/currency_exchange_service/controller/CircuitBreakerController.java
+++ src/main/java/com/urkejov/currency_exchange_service/controller/CircuitBreakerController.java
@@ -1,10 +1,7 @@
 package com.urkejov.currency_exchange_service.controller;
 
 import io.github.resilience4j.bulkhead.annotation.Bulkhead;
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
-import io.github.resilience4j.retry.annotation.Retry;
-import org.hibernate.annotations.DialectOverride;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
