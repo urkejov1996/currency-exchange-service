9b6a7abb109d75b17e09cefecbff3c05709024a6
urkejov1996 <urkejov1996@gmail.com>
2024-06-21 19:36:44 +0200

    //

--- src/main/java/com/urkejov/currency_exchange_service/controller/CircuitBreakerController.java
+++ src/main/java/com/urkejov/currency_exchange_service/controller/CircuitBreakerController.java
@@ -2,6 +2,8 @@ package com.urkejov.currency_exchange_service.controller;
 
 import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -13,9 +15,9 @@ import org.springframework.web.client.RestTemplate;
 public class CircuitBreakerController {
     @GetMapping("/sample-api")
 //     @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
-//    @CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
-//    @RateLimiter(name = "default")
-    @Bulkhead(name = "default")
+    @CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
+    @RateLimiter(name = "default")
+//    @Bulkhead(name = "default")
     public String sampleApi() {
         logger.info("Sample Api call received");
         ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-url", String.class);
